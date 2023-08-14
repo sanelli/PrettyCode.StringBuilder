3c599d154732d0c92917d03a2818f65e1eac2e6a
Stefano Anelli <stefano.anelli@gmail.com>
2023-08-14 22:53:10 +0100

    Curly braces code block

--- CodeStringBuilder/BuilderExtensions.cs
+++ CodeStringBuilder/BuilderExtensions.cs
@@ -18,4 +18,13 @@ public static class BuilderExtensions
     /// <returns>A disposable object that can be used to go back to previous indentation.</returns>
     public static IDisposable Indent(this Builder builder)
         => new Indentation(builder);
+
+    /// <summary>
+    /// Start a curly braces block.
+    /// </summary>
+    /// <param name="builder">The builder on which apply the indentation.</param>
+    /// <param name="addSemicolonAfterClose"><c>true</c> if a semicolon should be added after the closing brace.</param>
+    /// <returns>A disposable object that can be used to close the curly braces block.</returns>
+    public static IDisposable CurlyBracesBlock(this Builder builder, bool addSemicolonAfterClose = false)
+        => new CurlyBracesBlock(builder, addSemicolonAfterClose);
 }
